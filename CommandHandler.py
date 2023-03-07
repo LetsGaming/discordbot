2e2071f38d727a32c65a75f44c20b684018bbd49
LetsGaming <bergmanncrafter@gmail.com>
2023-03-07 17:36:45 +0100

    .

--- CommandHandler.py
+++ CommandHandler.py
@@ -1,7 +1,6 @@
 
 import discord
 from discord import app_commands
-from Commands.BotTools import Tools
 
 from Commands.ModerationCommands import Moderation
 from Commands.QuoteCommands import Quote
@@ -13,20 +12,19 @@ from Commands.ConvertCommands import Conversion
 
 class Commands():
     def __init__(self, client: discord.Client):
-        self.tree = app_commands.CommandTree(client=client) #Creates a new CommandTree to work with discods slash-commands
-        self.botTools = Tools(client=client)
+        self.tree = app_commands.CommandTree(client=self.client) #Creates a new CommandTree to work with discods slash-commands
         self.moderationCommands = Moderation()
         self.redditCommand = Reddit()
         self.weatherCommand = Weather()
-        self.quoteCommands = Quote(client=client)
+        self.quoteCommands = Quote(client=self.client)
         self.websiteCommand = WebsiteUtils()
         self.conversionCommand = Conversion()
-        self.ticketSystem = TicketSystem(tree=self.tree, client=client)
-        self.synced = False #Sets the synced bool to false
+        self.ticketSystem = TicketSystem(tree=self.tree, client=self.client)
         self.client = client
+        self.synced = False
+        self.tree.command(name="reload", description="Reloads all the bot commands.")(self.reload)
         
     async def register_commands(self):
-        self.tree.command(name="restart", description="Lets the bot owner restart the bot")(self.botTools.restart)
         self.tree.command(name="reddit", description="Sends a random post from the given subreddit.")(self.redditCommand.reddit) #Adds a new Command to the tree with the name and a short description, also adds the def that gets called when the command is triggered
         self.tree.command(name="clear", description="Deletes the given amount of messages.")(self.moderationCommands.clear_messages)
         self.tree.command(name="weather", description="Gets information about the weather in the given city.")(self.weatherCommand.getWeather)
@@ -40,4 +38,25 @@ class Commands():
         if not self.synced: #Checks if the commands aren't already synced
             await self.tree.sync() #Syncs the commands with discords server
             self.synced = True #Sets the synced varaible to true
-            
+
+    async def reload(self, interaction: discord.Interaction):
+        if interaction.user.id == 272402865874534400:
+            # Unregister all commands
+            self.tree.clear_commands()
+
+            # Re-initialize all commands
+            self.tree = app_commands.CommandTree(client=self.client) #Creates a new CommandTree to work with discods slash-commands
+            self.moderationCommands = Moderation()
+            self.redditCommand = Reddit()
+            self.weatherCommand = Weather()
+            self.quoteCommands = Quote(client=self.client)
+            self.websiteCommand = WebsiteUtils()
+            self.conversionCommand = Conversion()
+            self.ticketSystem = TicketSystem(tree=self.tree, client=self.client)
+
+            # Re-register all commands
+            self.synced = False
+            await self.register_commands() 
+        else:
+            interaction.channel.send("You're not the bot owner!")
+     
